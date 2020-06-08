111f9da72114fd4f9efaec3eaa0029690a72af8e
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 14:11:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 3 exercice 1</title>
+</head>
+<body>
+    <?php
+        $nb1 = 0;
+        $nb2 = 4;
+        while ($nb3 < 20) {
+            $somme = $nb1 * $nb2;
+            echo '<p>' . $somme . '</p>';
+            $nb1++;
+        }
+    ?>
+</body>
+</html>
