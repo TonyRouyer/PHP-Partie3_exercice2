c4ee6479ff5abeb35fbf25b970fdecfd855cedf4
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 11:35:59 +0200

    correction

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>partie 3 exercice 1</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>partie 3 exercice 2</title>
 </head>
 <body>
     <?php
         $nb1 = 0;
         $nb2 = 4;
-        while ($somme < 20) {
-            $somme = $nb1 * $nb2;
-            echo '<p>' . $somme . '</p>';
+        while ($nb1 < 20) {
+            $nb1 = $nb1 * $nb2;
+            echo '<p>' . $nb1 . '</p>';
             $nb1++;
         }
     ?>
